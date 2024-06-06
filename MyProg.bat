9447dd587d8f588fb232bea9be2af432ba21cce0
Andrew <an.erma4kov@yandex.ru>
2024-06-06 18:31:22 +0300

    123

---
+++ MyProg.bat
@@ -0,0 +1,22 @@
+echo off
+md Ermachkov
+cd Ermachkov
+md Andrew
+md Ermachkov
+pause
+cd Andrew
+echo > 01.txt
+cd..
+cd Ermachkov
+echo > 26112003.txt
+cd..
+cd..
+pause
+del Ermachkov /S /Q /F
+pause
+cd Ermachkov
+rd Ermachkov
+rd Andrew
+cd..
+rd Ermachkov
+pause
